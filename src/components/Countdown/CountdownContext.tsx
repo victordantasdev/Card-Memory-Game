1911b187e333dc68f48b87436d8a306ea00c30c1
victordantasdev <victordantasdeveloper@gmail.com>
2021-12-24 14:11:46 -0300

    feat: statistics from each level created

--- src/components/Countdown/CountdownContext.tsx
+++ src/components/Countdown/CountdownContext.tsx
@@ -9,20 +9,33 @@ interface CountdownContextData {
   win: boolean;
   minutes: number;
   seconds: number;
-  bestTime: string;
-  winTimes: number;
-  lostTimes: number;
+
+  bestTimeEasy: string;
+  lostTimesEasy: number;
+  winTimesEasy: number;
+
+  bestTimeMedium: string;
+  lostTimesMedium: number;
+  winTimesMedium: number;
+
+  bestTimeHard: string;
+  lostTimesHard: number;
+  winTimesHard: number;
+
   isActive: boolean;
   hasFinished: boolean;
   minuteLeft: string;
   minuteRight: string;
   secondLeft: string;
   secondRight: string;
-  stopCountdown: () => void;
-  resetCountdown: (n?: number) => void;
-  startCountdown: () => void;
+  resetCountdown: (n?: number, l?: string) => void;
+  stopCountdown: (level?: string | null | undefined) => void;
+  startCountdown: (t: number) => void;
   setWin: Dispatch<SetStateAction<boolean>>;
-  setWinTimes: Dispatch<SetStateAction<number>>;
+
+  setWinTimesEasy: Dispatch<SetStateAction<number>>;
+  setWinTimesMedium: Dispatch<SetStateAction<number>>;
+  setWinTimesHard: Dispatch<SetStateAction<number>>;
 }
 
 interface CountdownProviderProps {
@@ -34,16 +47,25 @@ export const CountdownConext = createContext({} as CountdownContextData);
 let countdownTimeout: NodeJS.Timeout;
 
 export function CountdownProvider({ children }: CountdownProviderProps) {
-  const fullTime = 5;
+  const [fullTime, setFullTime] = useState(5);
   const cookies = parseCookies();
 
   const [time, setTime] = useState(fullTime * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
-  const [bestTime, setBestTime] = useState(cookies.BEST_TIME || '00:00');
-  const [lostTimes, setLostTimes] = useState(Number(cookies.LOST_TIMES) || 0);
-  const [winTimes, setWinTimes] = useState(Number(cookies.WIN_TIMES) || 0);
   const [win, setWin] = useState(false);
+
+  const [bestTimeEasy, setBestTimeEasy] = useState(cookies.BEST_TIME_EASY || '00:00');
+  const [lostTimesEasy, setLostTimesEasy] = useState(Number(cookies.LOST_TIMES_EASY) || 0);
+  const [winTimesEasy, setWinTimesEasy] = useState(Number(cookies.WIN_TIMES_EASY) || 0);
+
+  const [bestTimeMedium, setBestTimeMedium] = useState(cookies.BEST_TIME || '00:00');
+  const [lostTimesMedium, setLostTimesMedium] = useState(Number(cookies.LOST_TIMES) || 0);
+  const [winTimesMedium, setWinTimesMedium] = useState(Number(cookies.WIN_TIMES) || 0);
+
+  const [bestTimeHard, setBestTimeHard] = useState(cookies.BEST_TIME_HARD || '00:00');
+  const [lostTimesHard, setLostTimesHard] = useState(Number(cookies.LOST_TIMES_HARD) || 0);
+  const [winTimesHard, setWinTimesHard] = useState(Number(cookies.WIN_TIMES_HARD) || 0);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -53,7 +75,9 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
 
   const cards = () => document.querySelectorAll('.card');
 
-  function startCountdown() {
+  function startCountdown(t: number) {
+    setFullTime(t);
+
     new Audio('/sounds/click.mp3').play();
     setIsActive(true);
     setWin(false);
@@ -70,7 +94,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     }, 1000);
   }
 
-  function resetCountdown(n: number = fullTime) {
+  function resetCountdown(n: number = fullTime, l: string = 'Easy') {
     new Audio('/sounds/click.mp3').play();
     clearTimeout(countdownTimeout);
     setIsActive(false);
@@ -84,20 +108,41 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
     });
   }
 
-  function stopCountdown() {
+  function stopCountdown(level: string | null | undefined) {
     clearTimeout(countdownTimeout);
     setIsActive(true);
     setTime(time);
     setHasFinished(false);
 
     const bestTimeStr = `${minuteLeft}${minuteRight}:${secondLeft}${secondRight}`;
-    if (bestTimeStr <= cookies.BEST_TIME || bestTimeStr !== '00:00') {
-      setBestTime(bestTimeStr);
 
-      setCookie(null, 'BEST_TIME', bestTimeStr, {
-        maxAge: 30 * 24 * 60 * 60,
-        path: '/',
-      });
+    if (level === 'Easy') {
+      if (bestTimeStr <= cookies.BEST_TIME_EASY || bestTimeStr !== '00:00') {
+        setBestTimeEasy(bestTimeStr);
+
+        setCookie(null, 'BEST_TIME_EASY', bestTimeStr, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
+      }
+    } else if (level === 'Medium') {
+      if (bestTimeStr <= cookies.BEST_TIME_MEDIUM || bestTimeStr !== '00:00') {
+        setBestTimeMedium(bestTimeStr);
+
+        setCookie(null, 'BEST_TIME_MEDIUM', bestTimeStr, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
+      }
+    } else if (level === 'Hard') {
+      if (bestTimeStr <= cookies.BEST_TIME_HARD || bestTimeStr !== '00:00') {
+        setBestTimeHard(bestTimeStr);
+
+        setCookie(null, 'BEST_TIME_HARD', bestTimeStr, {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
+      }
     }
   }
 
@@ -126,11 +171,26 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
         card.classList.remove('-match');
       });
 
-      setLostTimes(Number(lostTimes) + 1);
-      setCookie(null, 'LOST_TIMES', String(lostTimes + 1), {
-        maxAge: 30 * 24 * 60 * 60,
-        path: '/',
-      });
+      const $level = document.querySelector('.-selected')?.textContent;
+      if ($level === 'Easy') {
+        setLostTimesEasy(Number(lostTimesEasy) + 1);
+        setCookie(null, 'LOST_TIMES_EASY', String(lostTimesEasy + 1), {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
+      } else if ($level === 'Medium') {
+        setLostTimesMedium(Number(lostTimesMedium) + 1);
+        setCookie(null, 'LOST_TIMES_MEDIUM', String(lostTimesMedium + 1), {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
+      } else if ($level === 'Hard') {
+        setLostTimesHard(Number(lostTimesHard) + 1);
+        setCookie(null, 'LOST_TIMES_HARD', String(lostTimesHard + 1), {
+          maxAge: 30 * 24 * 60 * 60,
+          path: '/',
+        });
+      }
     }
   }, [isActive, time]);
 
@@ -140,16 +200,30 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
       minutes,
       seconds,
       isActive,
-      bestTime,
-      winTimes,
-      lostTimes,
+
+      bestTimeEasy,
+      lostTimesEasy,
+      winTimesEasy,
+
+      bestTimeMedium,
+      lostTimesMedium,
+      winTimesMedium,
+
+      bestTimeHard,
+      lostTimesHard,
+      winTimesHard,
+
       hasFinished,
       minuteLeft,
       minuteRight,
       secondLeft,
       secondRight,
       setWin,
-      setWinTimes,
+
+      setWinTimesEasy,
+      setWinTimesMedium,
+      setWinTimesHard,
+
       startCountdown,
       resetCountdown,
       stopCountdown,
